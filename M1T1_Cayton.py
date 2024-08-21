794ebf0621c7534e263e855b1d5b76a009b109ea
MaxCaytonFTCC <90518913+MaxCaytonFTCC@users.noreply.github.com>
2024-08-21 12:19:54 -0400

    Add files via upload

---
+++ M1T1_Cayton.py
@@ -0,0 +1,19 @@
+# CTS 285
+# M1T1
+# Max Cayton
+# 08/19/2024
+
+def displayText():
+    print('First Name: Max\nLast Name: Cayton\nLanguages I am Studying This Semester: C++ & likely Python with Flask for this class\n')
+    print('Preferred Language(s): C# & GDScript because I love the Godot Game Engine\n')
+    print('Additional Information: I am currently developing a 2D Sandbox RPG game while I work on getting my Bachelor\'s Degree\n')
+
+def getTerminationInput():
+    input('Press Enter to Terminate the Program: ')
+
+def main():
+    displayText()
+    getTerminationInput()
+
+
+main()
