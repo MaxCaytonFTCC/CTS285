dbbc11ac6b1fa064fd5185eb1a9b5b41701e6f66
MaxCaytonFTCC <90518913+MaxCaytonFTCC@users.noreply.github.com>
2024-09-16 17:00:26 +0000

    M2T2 Form Submission Works

--- M2T1/hello.py
+++ M2T1/hello.py
@@ -1,14 +1,20 @@
 # Minimal Flask App
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+# TODO Make this a database
+comments = []
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
     # Find template from /templates
     #name = "John"
-    return render_template("main_page.html")
+    if request.method == "GET":
+        return render_template("main_page.html", comments=comments)
+
+    comments.append(request.form["contents"])
+    return redirect(url_for('index'))
 
 @app.route("/action")
 def action():
